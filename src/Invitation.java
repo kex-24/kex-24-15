5a6768539cbe31d50c16c23e5bf7a2150d4f1be0
haaker1 <104437718+haaker1@users.noreply.github.com>
2024-04-10 16:59:45 +0200

    Create Invitation.java

---
+++ src/Invitation.java
@@ -0,0 +1,23 @@
+public class Invatation {
+    private Fika f;
+    private String name;
+
+    
+    public Invatation(Fika f, String name) {
+        this.f = f;
+        this.name = name;
+    }
+
+
+    public Fika getF() {
+        return f;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    
+
+}
